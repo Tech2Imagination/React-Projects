eb5c26d744d7688ff8f9205da441cd8708a3eecc
Ram chaudhary <88532722+Tech2Imagination@users.noreply.github.com>
2023-04-06 11:56:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+let time = new Date();
+time = time.getHours();
+const clock = new Date().toLocaleTimeString();
+let text = '';
+const cssStyle = {};
+if(time >= 1 && time < 12){
+  text = 'Good Morning';
+  cssStyle.color = 'yellow';
+}
+else if(time >= 12 && time < 19)
+{
+  text = 'Good Afternoon';
+  cssStyle.color = 'orange';
+}
+else{
+  text = 'Good Night';
+  cssStyle.color = 'white';
+}
+
+
+ReactDOM.render(
+  <>
+  <div className="box">
+  <div className="container">
+  <h1>Hello Sir,<span style={cssStyle}>{text} </span> </h1>
+  <p className="clock">{clock}</p>
+  </div>
+  </div>
+  </>,
+  document.getElementById('root')
+);
